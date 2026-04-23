59b67c784430963106e5386696f6fdf9ba3775aa
nmustakim <nmustakim98@gmail.com>
2026-04-23 23:55:24 +0600

    reverse

--- lib/core/constants/route_constants.dart
+++
@@ -1,8 +0,0 @@
-class Routes {
-  Routes._();
-  static const String splash   = '/';
-  static const String home     = '/home';
-  static const String result   = '/result';
-  static const String history  = '/history';
-  static const String settings = '/settings';
-}
